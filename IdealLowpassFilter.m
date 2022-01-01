31641206cf8b762654605e786d4611f439856018
ahmedheiba1999 <62450485+ahmedheiba1999@users.noreply.github.com>
2022-01-01 03:26:18 +0200

    Update IdealLowpassFilter.m

--- IdealLowpassFilter.m
+++ IdealLowpassFilter.m
@@ -1,4 +1,11 @@
-function y = IdealLowpassFilter(signal,fc,Fs)
-    %TO-DO         
+   function y = IdealLowpassFilter(signal,fc,Fs)
+hlp = (fc/pi) * sinc (fc*n/pi);
+
+Hlp = zeros(size(Fs));
+
+Hlp(-wc<=w & <= wc) = 1;
+    
+
+   
 end
 
