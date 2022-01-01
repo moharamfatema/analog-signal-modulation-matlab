c74a7c2c9fdaa666806a072a8dea0518477c1ede
ahmedheiba1999 <62450485+ahmedheiba1999@users.noreply.github.com>
2022-01-01 03:31:37 +0200

    Update IdealLowpassFilter.m

--- IdealLowpassFilter.m
+++ IdealLowpassFilter.m
@@ -1,10 +1,28 @@
    function y = IdealLowpassFilter(signal,fc,Fs)
-hlp = (fc/pi) * sinc (fc*n/pi);
 
-Hlp = zeros(size(Fs));
 
-Hlp(-wc<=w & <= wc) = 1;
-    
+x=audioread('eric.wav')';
+x=transpose(x);
+
+fs=48000; %sampling freq  
+N=411248;  % no of samples
+Ts=1/fs;     
+n=0:1:(N-1);
+t=n*Ts;
+k=-N/2:1:N/2-1;
+f=(fs/N)*k;
+
+xf=fftshift(fft(x));   %fourier transform of x(t)/X(f)
+xfm=abs(xf);           %|X(f)|
+
+
+Hf=[zeros(1,171354) ones(1,68541) zeros(1,171353)];   %H(f) LPF in freq domain
+figure;plot(f,Hf);grid on; title('filter in frequency domain H(f) vs f'); ylim([0 1.2]);
+
+
+yf=Hf.*xf;        % Y(f)=H(f).X(f)
+yfm=abs(yf);      % |Y(f)|
+y=ifft(ifftshift(yf));
 
    
 end
